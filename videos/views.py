ca366d8e987c861d9c0bc02923ca50c0ad7a05f3
Pranav0210 <pranavdev.618@gmail.com>
2025-05-26 21:24:56 +0530

    [feat]: added basic paginated search endpoint

--- videos/views.py
+++ videos/views.py
@@ -3,6 +3,8 @@ from rest_framework.generics import ListAPIView
 from .models import YouTubeVideo
 from .serializers import YouTubeVideoSerializer
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 
 # Create your views here.
 
@@ -11,3 +13,20 @@ class YouTubeVideoListView(ListAPIView):
     serializer_class = YouTubeVideoSerializer
     filter_backends = [SearchFilter]
     search_fields = ['title', 'description']
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+class YouTubeVideoSearchAPIView(ListAPIView):
+    serializer_class = YouTubeVideoSerializer
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        query = self.request.query_params.get("query", "")
+        if query:
+            return YouTubeVideo.objects.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            ).order_by("-published_at")
+        return YouTubeVideo.objects.none()
